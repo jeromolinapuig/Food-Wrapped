5448bbe22f2ef2ea30b701d98fb02820049274e3
jeromolinapuig <al398849@uji.es>
2026-01-09 14:31:06 +0100

    feat: add @vercel/analytics for enhanced analytics tracking in the application

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
